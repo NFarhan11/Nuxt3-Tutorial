75d641e3da225720e8c93e860c8c9b312c97139b
NFarhan11 <najmifarhan9903@gmail.com>
2024-11-21 06:40:37 +0800

    error handling & metadata

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,14 +1,27 @@
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Fugit asperiores aliquam distinctio doloribus ad molestiae, 
-            cupiditate architecto aperiam aliquid nobis!</p>
+        <Head>
+            <Title>Nuxt Dojo | {{ product.title }}</Title>
+            <Meta name="description" :content="product.description"></Meta>
+        </Head>
+        <ProductDetails :product="product"></ProductDetails>
     </div>
 </template>
 
 <script setup>
     const { id } = useRoute().params
+    const uri = 'https://fakestoreapi.com/products/' + id
+
+    // fetch the product
+    const { data: product } = await useFetch(uri, { key: id })
+
+    if (!product.value) {
+        throw createError({
+            statusCode: 404, 
+            statusMessage: 'Product not found.',
+            fatal: true
+        })
+    }
     
     definePageMeta({
         layout: 'products'
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <h2>Products</h2>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <ProductCard :product="p"></ProductCard>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
     definePageMeta({
         layout: 'products'
+    })
+
+    // fetch products
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+    useHead({
+        title: 'Nuxt Dojo | Merch',
+        meta: [
+            {
+                name: 'description', content: 'Nuxt 3 Merch'
+            }
+        ]
     })
 </script>
 
